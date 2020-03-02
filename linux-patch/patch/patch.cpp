9b91496232a3ff536a2604a9746ea803413d690f
pxie <pxie@grandstream.cn>
2020-03-03 01:58:37 +0800

    [NBF] Update

--- linux-patch/patch/patch.cpp
+++ linux-patch/patch/patch.cpp
@@ -1,14 +1,37 @@
 #include <iostream>
 #include <memory>
 #include <fstream>
+#include <unistd.h>
+#include <sys/ptrace.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <cstring>
 
 #include <bfd.h>
 
 #include <patch.h>
 #include <logger.h>
 
+std::string *symaddr::symaddr_get_name(void) const
+{
+    return name;
+}
+
+int symaddr::symaddr_get_addr(void) const
+{
+    return addr;
+}
+
+void symaddr::symaddr_show(void) const
+{
+    plogger(log_debug, "%s = %p[%p]\n", name->c_str(), (void *)addr, name);
+}
+
 symaddr::symaddr(std::string *name, int addr) : name(name)
-{}
+{
+    //plogger(log_debug, "%s = %p[%p]\n", name->c_str(), (void *)addr, name);
+    this->addr = addr;
+}
 
 symaddr::~symaddr()
 {}
@@ -53,8 +76,8 @@ int patch::patch_read_symbols(bfd *abfd, int offset)
         if (bfd_is_undefined_symclass(symclass)) {
             continue;
         }
-        plogger(log_debug, "%s = %p\n", sym_name, sym_value);
-        symaddrs.push(new symaddr(new std::string(sym_name), sym_value));
+        //plogger(log_debug, "%s = %p\n", sym_name, sym_value);
+        symaddrs.push_back(new symaddr(new std::string(sym_name), sym_value));
     }
     //plogger(log_debug, "storage_needed[%d], number_of_symbols[%d]\n", storage_needed, number_of_symbols);
 
@@ -97,8 +120,8 @@ dynsym:
             continue;
         }
 
-        plogger(log_debug, "%s = %p\n", sym_name, sym_value);
-        symaddrs.push(new symaddr(new std::string(sym_name), sym_value));
+        //plogger(log_debug, "%s = %p\n", sym_name, sym_value);
+        symaddrs.push_back(new symaddr(new std::string(sym_name), sym_value));
     }
 
 out:
@@ -110,12 +133,12 @@ out:
     return ret;
 }
 
-int patch::patch_symbol_init(pid_t pid, const char *filename)
+int patch::patch_symbol_init(pid_t pid, std::string &filename)
 {
     char buf[4096] = {0};
     bfd *abfd = NULL;
 
-    plogger(log_debug, "target symbol init, pid[%d] filename[%s]\n", pid, filename);
+    plogger(log_debug, "target symbol init, pid[%d] filename[%s]\n", pid, filename.c_str());
 
     snprintf(buf, sizeof(buf), "/proc/%d/maps", pid);
 
@@ -148,7 +171,7 @@ int patch::patch_symbol_init(pid_t pid, const char *filename)
         }
     }
 
-    abfd = bfd_openr(filename, NULL);
+    abfd = bfd_openr(filename.c_str(), NULL);
     if (nullptr == abfd) {
         plogger(log_warning, "bfd_openr failure!\n");
         return -1;
@@ -156,8 +179,144 @@ int patch::patch_symbol_init(pid_t pid, const char *filename)
     bfd_check_format(abfd, bfd_object);
     patch_read_symbols(abfd, 0);
     bfd_close(abfd);
-
     proc_map.close();
+
+    targpid = pid;
+    targname = filename;
+
+    return 0;
+}
+
+int patch::patch_attach_targprocess(void)
+{
+    if (ptrace(PTRACE_ATTACH, targpid, NULL, NULL) < 0) {
+        plogger(log_error, "Attached to target '%d' failure!\n", targpid);
+        return -1;
+    }
+
+    plogger(log_debug, "Attached to target '%d' success!\n", targpid);
+    wait(NULL);
+
+    return 0;
+}
+
+int patch::patch_detach_targprocess(void)
+{
+    ptrace(PTRACE_DETACH, targpid, NULL, NULL);
+
+    return 0;
+}
+
+int patch::patch_parse_command(std::string &command)
+{
+    int ret = 0;
+    char targsrcsymbol[1024] = {0};
+    char targdstsymbol[1024] = {0};
+
+    if (strstr(command.c_str(), "jmp")) {
+        if (sscanf(command.c_str(), "jmp %s %s\n", targsrcsymbol, targdstsymbol) != 2) {
+            return -1;
+        }
+
+        targsrcaddrinfo = patch_lookup_symaddr(targsrcsymbol);
+        if (!targsrcaddrinfo) {
+            plogger(log_debug, "symbol '%s' not found, targsrcaddrinfo[%p]\n", targsrcsymbol, (void *)targsrcaddrinfo);
+            return -1;
+        }
+
+        targdstaddrinfo = patch_lookup_symaddr(targdstsymbol);
+        if (!targdstaddrinfo) {
+            plogger(log_debug, "symbol '%s' not found, targsrcaddrinfo[%p]\n", targsrcsymbol, (void *)targsrcaddrinfo);
+            return -1;
+        }
+
+        targoffset = targdstaddrinfo - targsrcaddrinfo - 5;
+
+        plogger(log_debug, "targsrcsymbol[%s], targdstsymbol[%s], targoffset[%d]\n", targsrcsymbol, targdstsymbol, targoffset);
+    } else if (strstr(command.c_str(), "dl")) {
+        return -1;
+    }
+
+    return ret;
+}
+
+int patch::patch_lookup_symaddr(const char *symbol)
+{
+    for (auto iter = symaddrs.begin(); iter != symaddrs.end(); iter++) {
+        if (!strcmp((*iter)->symaddr_get_name()->c_str(), symbol)) {
+            (*iter)->symaddr_show();
+            return (*iter)->symaddr_get_addr();
+        }
+    }
+
+    return 0;
+}
+
+int patch::patch_set_data(void)
+{
+    char jmpbuf[5] = {0};
+#if 0
+    int addr0 = 0;
+    int len = 0;
+    int *lv = NULL;
+#endif
+
+    jmpbuf[0] = 0xe9;
+    memcpy(&jmpbuf[1], &targoffset, sizeof(int));
+
+    if (set_data(targpid, targsrcaddrinfo, jmpbuf, sizeof(jmpbuf))) {
+        return -1;
+    }
+
+#if 0
+    addr0 = targsrcaddrinfo & (~3);
+    len = (((targsrcaddrinfo + sizeof(jmpbuf)) - addr0) + 3) / 4;
+    lv = new int[sizeof(jmpbuf) * sizeof(int)];
+
+    for (int i = 0; i < len; i++) {
+        lv[i] = ptrace(PTRACE_PEEKDATA, targpid, addr0 + i * sizeof(int), NULL);
+        if (-1 == lv[i] && 0 != errno) {
+            plogger(log_error, "ptrace PTRACE_PEEKDATA failure, targpid[%d]!\n", targpid);
+            return -1;
+        }
+    }
+
+    memcpy((char *)lv + (targsrcaddrinfo - addr0), jmpbuf, sizeof(jmpbuf));
+
+    for (int i = 0; i < len; i++) {
+        lv[i] = ptrace(PTRACE_PEEKDATA, targpid, addr0 + i * sizeof(int), NULL);
+        if (-1 == lv[i] && 0 != errno) {
+            plogger(log_error, "ptrace PTRACE_PEEKDATA failure!\n");
+            return -1;
+        }
+    }
+#endif
+
+    return 0;
+}
+
+int patch::set_data(pid_t pid, int addr, void *val, int vlen)
+{
+    int i;
+    int addr0 = addr & ~3;
+    int len = (((addr + vlen) - addr0) + 3)/4;
+    int *lv = (int *)malloc(len * sizeof(int));
+
+    for (i = 0; i < len; i++) {
+        lv[i] = ptrace(PTRACE_PEEKDATA, pid, addr0 + i * sizeof(int), NULL);
+        if (lv[i] == -1 && errno != 0) {
+            perror("ptrace peek");
+            return -1;
+        }
+    }
+
+    memcpy((char *)lv + (addr - addr0), val, vlen);
+    for (i = 0; i < len; i++) {
+        if (ptrace(PTRACE_POKEDATA, pid, addr0 + i * sizeof(int), lv[i]) < 0) {
+            perror("ptrace poke");
+            return -1;
+        }
+    }
 
     return 0;
 }
@@ -170,11 +329,80 @@ patch::patch()
 patch::~patch()
 {}
 
-int main(int argc, const char **argv)
+static void patch_usage(char *prog)
 {
+    fprintf(stdout, "Usage:\n"
+        "\t %s -p <target-process-pid> -s <target-process-binary-file> -c <command> [-d dl]\n\n"
+        "\t dl: Represent dynamic library\n\n", prog);
+}
+
+int main(int argc, char * const *argv)
+{
+    int flags = 0;
+    int c;
+    pid_t targpid = - 1;
+    std::string targname;
+    std::string destdl;
+    std::string command;
+    int ret = 0;
+
+    while (-1 != (c = getopt(argc, argv, "p:s:d::c:"))) {
+        switch (c) {
+        case 'p':
+            targpid = atoi(optarg);
+            if (targpid <= 0) {
+                plogger(log_warning, "Invalid pid[%d], please check it!\n", targpid);
+                return -1;
+            }
+            flags |= patch::patch_targpid;
+            break;
+        case 's':
+            targname = std::string(optarg);
+            flags |= patch::patch_targbin;
+            break;
+        case 'd':
+            destdl = std::string(optarg);
+            break;
+        case 'c':
+            command = std::string(optarg);
+            flags |= patch::patch_command;
+            break;
+        default:
+            break;
+        }
+    }
+
+    if (!(flags & patch::patch_total)) {
+        patch_usage(argv[0]);
+        return -1;
+    }
+
     std::unique_ptr<patch> init(new patch);
 
-    init->patch_symbol_init(16570, "/opt/asterisk/sbin/asterisk");
+    if (init->patch_symbol_init(targpid, targname)) {
+        plogger(log_error, "patch symbol init failure!\n");
+        return -1;
+    }
 
-    return 0;
+    if (init->patch_attach_targprocess()) {
+        plogger(log_error, "patch attach target process failure!\n");
+        return -1;
+    }
+
+    if (init->patch_parse_command(command)) {
+        plogger(log_error, "patch parse command failure!\n");
+        ret = -1;
+        goto finished;
+    }
+
+    if (init->patch_set_data()) {
+        plogger(log_error, "patch set memory data failure!\n");
+        ret = -1;
+        goto finished;
+    }
+
+finished:
+    init->patch_detach_targprocess();
+
+    return ret;
 }
--- linux-patch/patch/patch.h
+++ linux-patch/patch/patch.h
@@ -1,12 +1,16 @@
 #ifndef __PATCH_H__
 #define __PATCH_H__
 
-#include <queue>
+#include <list>
 
 class symaddr {
 public:
     symaddr(std::string    *name, int addr);
     ~symaddr();
+
+    std::string *symaddr_get_name(void) const;
+    int symaddr_get_addr(void) const;
+    void symaddr_show(void) const;
 private:
     std::string *name;
     int addr;
@@ -14,13 +18,31 @@ private:
 
 class patch {
 public:
+    enum {
+        patch_targpid = 0 << 1,
+        patch_targbin = 1 << 1,
+        patch_command = 1 << 2,
+        patch_total   = patch_targpid | patch_targbin | patch_command,
+    };
+
     patch();
     ~patch();
 
-    int patch_symbol_init(pid_t pid, const char *filename);
+    int patch_symbol_init(pid_t pid, std::string &filenames);
     int patch_read_symbols(bfd *abfd, int offset);
+    int patch_attach_targprocess(void);
+    int patch_detach_targprocess(void);
+    int patch_parse_command(std::string &command);
+    int patch_lookup_symaddr(const char *symbol);
+    int patch_set_data(void);
 private:
-    std::queue<symaddr *> symaddrs;
+    int set_data(pid_t pid, int addr, void *val, int vlen);
+    std::list<symaddr *> symaddrs;
+    pid_t targpid;
+    std::string targname;
+    int targsrcaddrinfo;
+    int targdstaddrinfo;
+    int targoffset;
 };
 
 #endif
